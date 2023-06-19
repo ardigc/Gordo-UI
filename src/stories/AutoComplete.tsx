05687656fa5ecaa0cd5f34a87d4815948dc2c33e
ardigc <agordo.95@gmail.com>
2023-06-20 00:31:43 +0200

    elegir option en autocomlete

--- src/stories/AutoComplete.tsx
+++ src/stories/AutoComplete.tsx
@@ -2,13 +2,20 @@
 import { useState } from 'react'
 export default function AutoComplete({ options }: { options: Array<string> }) {
   const [open, setOpen] = useState(false)
+  const [value, onValue] = useState('Options')
   return (
-    <div>
-      <input className="border" onClick={() => setOpen(!open)}></input>
+    <div onClick={() => setOpen(!open)}>
+      <input value={value} className="border"></input>
       {open && (
         <div>
-          {options.map((option) => (
-            <div>{option}</div>
+          {options.map((option, index) => (
+            <div
+              onClick={() => onValue(option)}
+              className="cursor-pointer hover:bg-slate-200"
+              key={index}
+            >
+              {option}
+            </div>
           ))}
         </div>
       )}
