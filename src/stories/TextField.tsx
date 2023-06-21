f133e04a69947e5d48953a24867f109cbd27d0c5
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-21 17:04:43 +0000

    onblur se ejecuta si hay contenido

--- src/stories/TextField.tsx
+++ src/stories/TextField.tsx
@@ -8,9 +8,10 @@ export interface TextField {
 }
 export default function TextField({ label, variant, classes }: TextField) {
   const [touched, setTouched] = useState(false)
+  const [value, setValue]= useState('')
   const labelClassName= classes?.labelClassName
   return (
-    <div className="relative inline-flex" onBlur={()=>setTouched(false)}>
+    <div className="relative inline-flex" onBlur={()=>{ if(value.length<1){setTouched(false)}}}>
       <label
         className={classNames('label-text-field','text-lg cursor-text' ,{
           'normal-label-text-field-filled': !touched,
@@ -22,9 +23,9 @@ export default function TextField({ label, variant, classes }: TextField) {
         {label}
       </label>
       <div className={classNames('input-div-filled after:border-b-2 after:border-b-blue-600',{'input-div-filled-none ':!touched, 'input-div-filled-normal':touched,[classes?.inputContainerClassName||'']: classes?.inputContainerClassName})}>
-        <input id='filled-input'  onFocus={() => setTouched(true)} className={classNames("input-text-field outline-none bg-gray-100 rounded-sm",{[classes?.inputClassName||'']: classes?.inputClassName})} />
+        <input id='filled-input' onChange={(ev)=>{setValue(ev.currentTarget.value)}} onFocus={() =>setTouched(true)} className={classNames("input-text-field outline-none bg-gray-100 rounded-sm",{[classes?.inputClassName||'']: classes?.inputClassName})} />
             </div>
-            {/* aklss */}
+           
     </div>
   )
 }
