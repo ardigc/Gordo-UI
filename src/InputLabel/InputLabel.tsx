cd830edf4ae9612eaa2b890d981017534ac9a4bd
ardigc <agordo.95@gmail.com>
2023-06-24 00:52:37 +0200

    disable animation

--- src/InputLabel/InputLabel.tsx
+++ src/InputLabel/InputLabel.tsx
@@ -4,16 +4,21 @@ export interface InputLabelProps {
   children?: ReactNode
   className?: string
   htmlFor?: string
+  disableAnimation?: boolean
 }
 export default function InputLabel({
   children,
   htmlFor,
   className,
+  disableAnimation,
 }: InputLabelProps) {
   return (
     <label
       htmlFor={htmlFor}
-      className={classNames({ [className || '']: className })}
+      className={classNames({
+        [className || '']: className,
+        'transition-all': !disableAnimation,
+      })}
     >
       {children}
     </label>
