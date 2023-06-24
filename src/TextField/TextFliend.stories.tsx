09816ea63f66d245051c04e9dd1b4a4f3bc7025b
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-24 16:37:26 +0000

    placeholder

--- src/TextField/TextFliend.stories.tsx
+++ src/TextField/TextFliend.stories.tsx
@@ -38,6 +38,7 @@ export const numeros: Story = {
   args: {
     type: 'number',
     disableUnderline: true,
+    placeholder:'hola',
     margin:'dense',
     onChange(ev) {
       console.log(ev.currentTarget.value)
