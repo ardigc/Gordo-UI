3513c65278646c5231e1f9cb055342c9d7df7fc0
ardigc <agordo.95@gmail.com>
2023-06-26 00:19:44 +0200

    fix(css): editing transparency custom colors

--- src/FormHelper/FormHelper.stories.tsx
+++ src/FormHelper/FormHelper.stories.tsx
@@ -6,8 +6,8 @@ const meta = {
   tags: ['autodocs'],
 } satisfies Meta<typeof FormHelper>
 export default meta
-
 type Story = StoryObj<typeof meta>
+
 export const primary: Story = {
   args: {
     children: 'holaasdasdasd asd asd asd asd asd as',
