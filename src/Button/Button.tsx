cb4914d14a2d9d864655d621e533d57d2e22702e
ardigc <agordo.95@gmail.com>
2023-06-26 02:10:37 +0200

    feat(button): add span with bugs

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { ReactNode, useState } from 'react'
+import { ReactNode, useState, MouseEventHandler } from 'react'
 
 export interface ButtonProps {
   children?: ReactNode
@@ -7,14 +7,24 @@ export interface ButtonProps {
 }
 export default function Button({ children, variant = 'text' }: ButtonProps) {
   const [animation, setAnimation] = useState(false)
+  const [clickCoord, setClickCoord] = useState<{ x: number; y: number }>()
+  const onClickHandler: MouseEventHandler<HTMLButtonElement> = (ev) => {
+    const target = ev.currentTarget
+    const location = target.getBoundingClientRect()
+    const coord = {
+      x: ev.clientX - location.left,
+      y: ev.clientY - location.top,
+    }
+    setClickCoord({ x: coord.x, y: coord.y })
+    console.log(coord)
+    setAnimation(true)
+    // setTimeout(() => {
+    //   setAnimation(false)
+    // }, 900)
+  }
   return (
     <button
-      onClick={() => {
-        setAnimation(true)
-        setTimeout(() => {
-          setAnimation(false)
-        }, 900)
-      }}
+      onClick={onClickHandler}
       className={classNames(
         'inline-flex items-center justify-center relative bg-transparent outline-none uppercase rounded-[4px] min-w-[64px]',
         'overflow-hidden',
@@ -25,7 +35,12 @@ export default function Button({ children, variant = 'text' }: ButtonProps) {
       )}
     >
       {animation && (
-        <span className="animate-ripple absolute inline-flex w-full h-full rounded-full bg-primary-color bg-opacity-25"></span>
+        <span
+          className={classNames(
+            'animate-ripple absolute inline-flex w-full h-full rounded-full bg-primary-color bg-opacity-25',
+            [`top-[${clickCoord?.y}px] left-[${clickCoord?.x}px]`]
+          )}
+        ></span>
       )}
       {children}
     </button>
