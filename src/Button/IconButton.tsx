c2ec8bebab5f36c784a0a5e0884aa9ad1c5566d7
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-26 18:50:25 +0000

    create animation iconbuton

--- src/Button/IconButton.tsx
+++ src/Button/IconButton.tsx
@@ -8,11 +8,19 @@ export interface IconButonProps{
     | 'large'
     onClick?: (ev: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void
     href?: string
-
+disableRipple?:boolean
+color?:
+| 'primary'
+| 'secondary'
+| 'error'
+| 'warning'
+| 'info'
+| 'success'
+| 'inherit'
 
 }
 
-export default function IconButton({children, size='medium', onClick, href}:IconButonProps) {
+export default function IconButton({children, size='medium', onClick, href, disableRipple, color='primary'}:IconButonProps) {
     const [animation, setAnimation] = useState(false)
     const [clickCoord, setClickCoord] = useState<{ x: number; y: number }>()
     // const isCustomcolor=color!==
@@ -36,7 +44,18 @@ export default function IconButton({children, size='medium', onClick, href}:Icon
     onClick(ev)
   }
   }
-    return(<button onClick={onClickHandler} className={classNames({'p-2':size==='medium', 'p-[5px]': size==='small','p-3':size==='large'})}>
+    return(<button onClick={onClickHandler} className={classNames(' inline-flex items-center  justify-center  relative overflow-hidden',{'p-2':size==='medium', 'p-[5px]': size==='small','p-3':size==='large', 'rounded-full':animation})}>
         {children}
+        {animation && (
+        <span
+          style={{
+            top: clickCoord?.y,
+            left: clickCoord?.x,
+            transform: 'translate(-50%, -50%)',
+          }}
+          className={classNames(
+            'animate-ripple absolute inline-flex w-full h-full bg-slate-200  rounded-full bg-opacity-25 origin-center')}
+            ></span>
+      )}
     </button>)
 }
