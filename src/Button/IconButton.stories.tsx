a347132af6d8b7ecfb6965e6abe9c1e322b46823
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-26 16:47:14 +0000

    creating icon button stories

---
+++ src/Button/IconButton.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import IconButton from './IconButton'
+const meta = {
+  component: IconButton,
+  title: 'GordoUI/IconButton',
+  tags: ['autodocs'],
+} satisfies Meta<typeof IconButton>
+export default meta
+type Story = StoryObj<typeof meta>
