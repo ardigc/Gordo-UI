052014b2db703ff25b0d375786318644a476e71b
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-27 15:41:41 +0000

    create story on popover

--- src/Popover/Popover.stories.tsx
+++ src/Popover/Popover.stories.tsx
@@ -19,7 +19,26 @@ export const primary: Story = {
     },
   },
 }
-const PopoverWithButton = () => {
+// const PopoverWithButton = () => {
+//   const [open, setOpen] = useState(false)
+//   const [anchorEl, setAnchorEl] = useState<Element | undefined>(undefined)
+//   const handleClick: MouseEventHandler<HTMLButtonElement> = (ev) => {
+//     setAnchorEl(ev.currentTarget)
+//     setOpen(true)
+//   }
+
+//   return (
+//     <>
+//       <Button onClick={handleClick}>click aqui</Button>
+//       <Popover onClose={() => setOpen(false)}  anchorEl={anchorEl} open={open} />
+//     </>
+//   )
+// }
+
+// export const test2: Story = {
+//   render: () => <PopoverWithButton />,
+// }
+const PopoverWithButtonBottom = ({args}:{args:any}) => {
   const [open, setOpen] = useState(false)
   const [anchorEl, setAnchorEl] = useState<Element | undefined>(undefined)
   const handleClick: MouseEventHandler<HTMLButtonElement> = (ev) => {
@@ -30,30 +49,17 @@ const PopoverWithButton = () => {
   return (
     <>
       <Button onClick={handleClick}>click aqui</Button>
-      <Popover onClose={() => setOpen(false)}  anchorEl={anchorEl} open={open} />
-    </>
-  )
-}
-
-export const test2: Story = {
-  render: () => <PopoverWithButton />,
-}
-const PopoverWithButtonBottom = () => {
-  const [open, setOpen] = useState(false)
-  const [anchorEl, setAnchorEl] = useState<Element | undefined>(undefined)
-  const handleClick: MouseEventHandler<HTMLButtonElement> = (ev) => {
-    setAnchorEl(ev.currentTarget)
-    setOpen(true)
-  }
-
-  return (
-    <>
-      <Button onClick={handleClick}>click aqui</Button>
-      <Popover onClose={() => setOpen(false)} anchorOrigin={{vertical:'bottom', horizontal:'left'}} anchorEl={anchorEl} open={open} />
+      <Popover onClose={() => setOpen(false)}  anchorEl={anchorEl} open={open} {...args} />
     </>
   )
 }
 
 export const test: Story = {
-  render: () => <PopoverWithButtonBottom />,
+  render: (args) => <PopoverWithButtonBottom args={args}  />,
+  args:{
+    open:false,
+  
+    anchorOrigin:{vertical:'bottom', horizontal:'left'}
+    
+  }
 }
