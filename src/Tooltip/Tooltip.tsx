cb5f5e546b354b4b0ae868520e85bb1767efe658
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-28 15:37:03 +0000

    styling tooltip

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -1,15 +1,20 @@
-import React , { ElementType, JSXElementConstructor, ReactElement, ReactNode, useEffect, useState } from 'react'
+import React , { ElementType, JSXElementConstructor, MouseEventHandler, ReactElement, ReactNode, useEffect, useState } from 'react'
 import Popover from '../Popover/Popover'
 export interface TooltipProps {
   children: ReactElement<any, string | JSXElementConstructor<any>>
 }
 export default function Tooltip({ children }: TooltipProps) {
+  const [anchorEl, setAnchorEl]=useState<Element|undefined>(undefined)
   const [open, setOpen]=useState(false)
-  return <div>
-    <Popover open={open} classes={{root:'pointer-events-none'}}>
-
+  const onMouseEnterHandler: MouseEventHandler<HTMLDivElement>=(ev)=>{
+setAnchorEl(ev.currentTarget)
+setOpen(true)
+  }
+  return <div className='inline-flex'>
+    <Popover open={open} anchorOrigin={{vertical:'bottom', horizontal:'center'}}  anchorEl={anchorEl} classes={{root:'pointer-events-none'}}>
+holiwi
     </Popover>
-    <div onMouseEnter={()=>setOpen(true)} onMouseLeave={()=>setOpen(false)}>
+    <div onMouseEnter={onMouseEnterHandler} onMouseLeave={()=>setOpen(false)}>
 
 {children}  
     </div>
