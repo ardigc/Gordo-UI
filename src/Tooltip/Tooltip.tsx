4873564fa7dda0c3013f8943eb43857dd054d8f0
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-28 13:52:10 +0000

    create structure tooltip

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -4,9 +4,12 @@ export interface TooltipProps {
   children: ReactElement<any, string | JSXElementConstructor<any>>
 }
 export default function Tooltip({ children }: TooltipProps) {
+  const [open, setOpen]=useState(false)
   return <div>
-    <Popover />
-    <div onMouseEnter={}>
+    <Popover open={open} classes={{root:'pointer-event-none'}}>
+
+    </Popover>
+    <div onMouseEnter={()=>setOpen(true)} onMouseLeave={()=>setOpen(false)}>
 
 {children}  
     </div>
