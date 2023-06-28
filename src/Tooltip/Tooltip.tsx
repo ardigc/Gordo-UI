72b76f9f4d2b94c5fe5ceb5778709a6440e6daaa
ardigc <130089987+ardigc@users.noreply.github.com>
2023-06-28 13:36:45 +0000

    starting tooltip

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -1,7 +1,14 @@
-import { ElementType, ReactNode, useEffect, useState } from 'react'
+import React , { ElementType, JSXElementConstructor, ReactElement, ReactNode, useEffect, useState } from 'react'
+import Popover from '../Popover/Popover'
 export interface TooltipProps {
-  children: ReactNode
+  children: ReactElement<any, string | JSXElementConstructor<any>>
 }
 export default function Tooltip({ children }: TooltipProps) {
-  return <div>{children}</div>
+  return <div>
+    <Popover />
+    <div onMouseEnter={}>
+
+{children}  
+    </div>
+</div>
 }
