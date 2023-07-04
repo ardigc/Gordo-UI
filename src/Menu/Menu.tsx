94e57b6ff9292f472d68549bf976f48c3942de22
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-04 15:48:32 +0000

    add bg and anchorel

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -3,11 +3,14 @@ import Popover from '../Popover/Popover'
 export interface MenuProps{
     children?:ReactNode
     open: boolean
+    anchorEl?: Element | (() => Element)
 }
-export default function Menu({children, open}:MenuProps) {
-  return( <Popover open={open}>
+export default function Menu({children, open, anchorEl}:MenuProps) {
+  return( 
+  <Popover anchorEl={anchorEl} className='bg-white' open={open}>
         <ul>
             {children}
         </ul>
-    </Popover>) 
+    </Popover>
+    ) 
 }
