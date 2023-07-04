c7e8a43af62e46ddc9d3952a95cc82ba96a6fbf9
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-04 14:48:57 +0000

    creating menu stories

---
+++ src/Menu/Menu.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Menu, { MenuProps } from './Menu'
+
+const meta = {
+  component: Menu,
+  title: 'GordoUI/Menu',
+  tags: ['autodocs'],
+} satisfies Meta<typeof Menu>
+export default meta
+type Story = StoryObj<typeof meta>
+
+const TestButtonFunc = ({ args }: { args: MenuProps }) => {
+    return (<>
+    <Menu >
+        
+    </Menu>
+    </>)
+}
+export const testButton: Story = {
+    render: (args) => <TestButtonFunc args={args} />,
+    args: {
+      children:<div></div>,
+      open:false,
+    }
+}
--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,13 +1,13 @@
 import {ReactNode} from 'react'
 import Popover from '../Popover/Popover'
 export interface MenuProps{
-    children:ReactNode
+    children?:ReactNode
     open: boolean
 }
 export default function Menu({children, open}:MenuProps) {
-    <Popover open={open}>
+  return( <Popover open={open}>
         <ul>
             {children}
         </ul>
-    </Popover>
+    </Popover>) 
 }
