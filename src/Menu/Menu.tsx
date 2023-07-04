3d19bd507bcf9e32b9bde98cf6b5e070fc4f05ae
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-04 14:35:09 +0000

    set open ando children on menu

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,7 +1,13 @@
 import {ReactNode} from 'react'
+import Popover from '../Popover/Popover'
 export interface MenuProps{
     children:ReactNode
+    open: boolean
 }
-export default function Menu({children}:MenuProps) {
-    
+export default function Menu({children, open}:MenuProps) {
+    <Popover open={open}>
+        <ul>
+            {children}
+        </ul>
+    </Popover>
 }
