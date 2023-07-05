9bebc93a1bd8ce1fc94f9ec966e7730bb1d9406b
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-05 15:12:55 +0000

    menu list props

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,7 +1,6 @@
-import {ReactNode, useEffect} from 'react'
+import {ReactNode} from 'react'
 import Popover from '../Popover/Popover'
-import Clickaway from '../ClickAway/ClickAway'
-import MenuList from '../MenuList/MenuList'
+import MenuList, { MenuListProps } from '../MenuList/MenuList'
 import classNames from 'classnames'
 export interface MenuProps{
     children?:ReactNode
@@ -9,8 +8,9 @@ export interface MenuProps{
     anchorEl?: Element | (() => Element)
     onClose?: ()=>void
     classes?:{Popover?:string, MenuList?: string}
+    menuListProps?: MenuListProps
 }
-export default function Menu({children, open, anchorEl ,onClose, classes}:MenuProps) {
+export default function Menu({children, open, anchorEl ,onClose, classes, menuListProps}:MenuProps) {
  const clickAwayHandler=()=>{
     if (onClose) {
         
@@ -20,7 +20,7 @@ export default function Menu({children, open, anchorEl ,onClose, classes}:MenuPr
     return( 
 
   <Popover anchorEl={anchorEl}  anchorOrigin={{vertical:'bottom', horizontal:'left'}} onClose={clickAwayHandler} className={classNames('bg-white', {[classes?.Popover||'']:classes?.Popover})} open={open}>
-        <MenuList className={classNames({[classes?.MenuList||'']:classes?.MenuList})}>
+        <MenuList {...menuListProps} className={classNames({[classes?.MenuList||'']:classes?.MenuList})}>
             {children}
         </MenuList>
     </Popover>
