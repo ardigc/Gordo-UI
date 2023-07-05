1fc25437841b890d4da50224fa5c5cb5c5f51ab0
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-05 16:40:36 +0000

    creating taablecell

---
+++ src/Table/TableCell.tsx
@@ -0,0 +1,8 @@
+import { DetailedHTMLProps, TdHTMLAttributes } from "react";
+
+type TableReactProps= DetailedHTMLProps<TdHTMLAttributes<HTMLTableDataCellElement>,HTMLTableDataCellElement>,
+export default function TableCell({}:TableCellProps) {
+    return (
+        <td></td>
+    )
+}
