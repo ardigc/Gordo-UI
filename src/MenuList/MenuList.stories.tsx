868f3dc41d14c28057bc75265feac4955f804fd0
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-05 13:51:39 +0000

    creating menulist stories

---
+++ src/MenuList/MenuList.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react'
+import MenuList, { MenuListProps } from './MenuList'
+import MenuItem from '../MenuItem/MenuItem'
+import Button from '../Button/Button'
+import { MouseEventHandler, useState } from 'react'
+
+const meta = {
+  component: MenuList,
+  title: 'GordoUI/MenuList',
+  tags: ['autodocs'],
+} satisfies Meta<typeof MenuList>
+export default meta
+type Story = StoryObj<typeof meta>
+
+const TestButtonFunc = ({ args }: { args: MenuListProps }) => {
+ 
+    return (<>
+    <div className="flex justify-center h-56 items-center">
+
+    <MenuList >
+    <MenuItem divider >opcion 1</MenuItem>
+    <MenuItem >opcion 2</MenuItem>
+    <MenuItem >opcion 3</MenuItem>
+    </MenuList>
+    </div>
+    </>)
+}
+export const testButton: Story = {
+    render: (args) => <TestButtonFunc args={args} />,
+    args: {
+      children:<div></div>,
+    }
+}
--- src/MenuList/MenuList.tsx
+++ src/MenuList/MenuList.tsx
@@ -1,12 +1,12 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react"
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react"
 
 type UlReactProps= DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement>
 export interface MenuListProps extends UlReactProps{
-
+children:ReactNode
 }
-export default function MenuList({...rest}:MenuListProps) {
+export default function MenuList({children,...rest}:MenuListProps) {
     return (<ul {...rest}>
-        
+        {children}
     </ul>)
     
 }
