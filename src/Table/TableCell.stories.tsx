e162cdac0234fb4e2e0a754a378c01e52878d626
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-05 17:56:55 +0000

    creating stories

---
+++ src/Table/TableCell.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import TableCell from './TableCell'
+import Button from '../Button/Button'
+import { MouseEventHandler, useState } from 'react'
+
+const meta = {
+  component: TableCell,
+  title: 'GordoUI/TableCell',
+  tags: ['autodocs'],
+} satisfies Meta<typeof TableCell>
+export default meta
+type Story = StoryObj<typeof meta>
+export const primary: Story = {
+  args: {
+    children: 'hola',
+    className: 'bg-slate-400',
+
+  },
+}
