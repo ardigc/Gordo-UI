ea2ab5c3a9b0414bf80926aff1964eb6ffe5c9fd
ardigc <agordo.95@gmail.com>
2023-07-07 00:09:53 +0200

    create table head stories

---
+++ src/Table/TableHead.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryObj } from '@storybook/react'
+import TableHead, { TableHeadProps } from './TableHead'
+import TableCell from './TableCell'
+import TableRow from './TableRow'
+
+const meta = {
+  component: TableHead,
+  title: 'GordoUI/TableHead',
+  tags: ['autodocs'],
+} satisfies Meta<typeof TableHead>
+export default meta
+type Story = StoryObj<typeof meta>
+
+const TestFunc = ({ args }: { args: TableHeadProps }) => {
+  return (
+    <>
+      <TableHead {...args}>
+        <TableRow>
+          <TableCell> hola</TableCell>
+          <TableCell> hola</TableCell>
+          <TableCell> hola</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell> hola</TableCell>
+          <TableCell> hola</TableCell>
+          <TableCell> hola</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell> hola</TableCell>
+          <TableCell> hola</TableCell>
+          <TableCell> hola</TableCell>
+        </TableRow>
+      </TableHead>
+    </>
+  )
+}
+export const test: Story = {
+  render: (args) => <TestFunc args={args} />,
+  args: {
+    className: 'rounded ',
+  },
+}
