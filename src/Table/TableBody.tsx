60381de066220488b4a2da0d1a9c40a7d0e9cdfc
ardigc <agordo.95@gmail.com>
2023-07-06 23:43:59 +0200

    creating tablebodyprops

--- src/Table/TableBody.tsx
+++ src/Table/TableBody.tsx
@@ -1,3 +1,10 @@
-export default function TableBody({}: type) {
+import { DetailedHTMLProps, HTMLAttributes } from 'react'
+
+type TableBodyReactProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLTableSectionElement>,
+  HTMLTableSectionElement
+>
+export interface TableBodyProps extends TableBodyReactProps {}
+export default function TableBody({}: TableBodyProps) {
   return <tbody></tbody>
 }
