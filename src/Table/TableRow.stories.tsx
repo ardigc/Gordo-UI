3cf204dfbd33899575100f0115665fae4ab616a4
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-06 15:33:53 +0000

    creating table row stories

--- src/Table/TableRow.stories.tsx
+++ src/Table/TableRow.stories.tsx
@@ -1,7 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react'
-import TableRow from './TableRow'
+import TableRow, { TableRowProps } from './TableRow'
 import Button from '../Button/Button'
 import { MouseEventHandler, useState } from 'react'
+import TableCell from './TableCell'
 
 const meta = {
   component: TableRow,
@@ -9,3 +10,20 @@ const meta = {
   tags: ['autodocs'],
 } satisfies Meta<typeof TableRow>
 export default meta
+type Story = StoryObj<typeof meta>
+
+const TestFunc = ({ args }: { args: TableRowProps }) => {
+    return(
+<>
+<TableRow {...args}>
+    <TableCell> hola</TableCell>
+</TableRow>
+</>
+)
+}
+export const test: Story = {
+  render: (args) => <TestFunc args={args} />,
+  args: {
+
+    },
+}
