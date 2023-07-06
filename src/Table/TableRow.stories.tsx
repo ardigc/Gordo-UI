6a2733e08e9e5c578d0cec37064e9e8021d83adc
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-06 15:25:15 +0000

    creating stories table row

---
+++ src/Table/TableRow.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import TableRow from './TableRow'
+import Button from '../Button/Button'
+import { MouseEventHandler, useState } from 'react'
+
+const meta = {
+  component: TableRow,
+  title: 'GordoUI/TableRow',
+  tags: ['autodocs'],
+} satisfies Meta<typeof TableRow>
+export default meta
