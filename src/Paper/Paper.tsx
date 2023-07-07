b8f57eab6fc89ee7cf2a254402ba5774c6d36395
ardigc <agordo.95@gmail.com>
2023-07-07 11:31:05 +0200

    add children

--- src/Paper/Paper.tsx
+++ src/Paper/Paper.tsx
@@ -1,9 +1,11 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 type PaperReactProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >
-export interface PaperProps extends PaperReactProps {}
-export default function Paper({}: PaperProps) {
-  return <div></div>
+export interface PaperProps extends PaperReactProps {
+  children: ReactNode
+}
+export default function Paper({ children }: PaperProps) {
+  return <div>{children}</div>
 }
