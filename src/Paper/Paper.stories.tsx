11f996696a3fc99bb6c24d6e4392a916762cc889
ardigc <agordo.95@gmail.com>
2023-07-07 11:36:53 +0200

    testing paperStories

---
+++ src/Paper/Paper.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Paper from './Paper'
+
+const meta = {
+  component: Paper,
+  title: 'GordoUI/Paper',
+  tags: ['autodocs'],
+} satisfies Meta<typeof Paper>
+export default meta
+type Story = StoryObj<typeof meta>
+export const primary: Story = {
+  args: {
+    children: <div>hola</div>,
+  },
+}
