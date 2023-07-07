4ce9b02f0c7b6b237ad50e434760135a1d44394e
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-07 14:07:09 +0000

    add props to collapse

--- src/Collapse/Collapse.tsx
+++ src/Collapse/Collapse.tsx
@@ -1,10 +1,13 @@
+import classNames from "classnames"
 import { ReactNode } from "react"
 
 export interface CollapseProps{
     children:ReactNode
+    in?:boolean
+    className?:string
 }
-export default function Collapse({children}:CollapseProps) {
-    return(<div>
+export default function Collapse({children, in, className }:CollapseProps) {
+    return(<div className={classNames()}>
 {children}
     </div>)
 }
