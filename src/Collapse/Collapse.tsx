360065493439d8d91e391ce22c03f938de53d094
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-07 15:04:20 +0000

    change in for open

--- src/Collapse/Collapse.tsx
+++ src/Collapse/Collapse.tsx
@@ -3,11 +3,11 @@ import { ReactNode } from "react"
 
 export interface CollapseProps{
     children:ReactNode
-    in?:boolean
+    open?:boolean
     className?:string
 }
-export default function Collapse({children, in, className }:CollapseProps) {
-    return(<div className={classNames()}>
+export default function Collapse({children, open, className }:CollapseProps) {
+    return(<div className={classNames({'h-auto':open===true, 'h-0':open===false})}>
 {children}
     </div>)
 }
