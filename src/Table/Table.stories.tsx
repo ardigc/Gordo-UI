0f3bddca350bd5442046c5b019708aa7a5efc2aa
ardigc <agordo.95@gmail.com>
2023-07-07 10:35:43 +0200

    testing table

--- src/Table/Table.stories.tsx
+++ src/Table/Table.stories.tsx
@@ -14,30 +14,49 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 const TestFunc = ({ args }: { args: TableProps }) => {
+  function createData(
+    name: string,
+    calories: number,
+    fat: number,
+    carbs: number,
+    protein: number
+  ) {
+    return { name, calories, fat, carbs, protein }
+  }
+
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ]
+
   return (
-    <>
-      <Table {...args}>
-        <TableHead>
-          <TableRow>
-            <TableCell> hola</TableCell>
-            <TableCell> hola</TableCell>
-            <TableCell> hola</TableCell>
+    <Table {...args}>
+      <TableHead>
+        <TableRow>
+          <TableCell>Dessert (100g serving)</TableCell>
+          <TableCell align="right">Calories</TableCell>
+          <TableCell align="right">Fat&nbsp;(g)</TableCell>
+          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+          <TableCell align="right">Protein&nbsp;(g)</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.name}>
+            <TableCell component="th" scope="row">
+              {row.name}
+            </TableCell>
+            <TableCell align="right">{row.calories}</TableCell>
+            <TableCell align="right">{row.fat}</TableCell>
+            <TableCell align="right">{row.carbs}</TableCell>
+            <TableCell align="right">{row.protein}</TableCell>
           </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell> hola</TableCell>
-            <TableCell variant="body"> hola</TableCell>
-            <TableCell> hola</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell> hola</TableCell>
-            <TableCell variant="footer"> hola</TableCell>
-            <TableCell> hola</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
 export const test: Story = {
