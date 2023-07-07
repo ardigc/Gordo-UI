b070a74fa4f185f2aa546a74eb6694603a5b7f0c
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-07 16:19:41 +0000

    testing clne children

--- src/Collapse/Collapse.tsx
+++ src/Collapse/Collapse.tsx
@@ -1,13 +1,18 @@
 import classNames from "classnames"
-import { ReactNode } from "react"
+import React, { JSXElementConstructor, ReactElement, ReactNode, useEffect, useRef } from "react"
 
 export interface CollapseProps{
-    children:ReactNode
+    children: ReactElement<any, string | JSXElementConstructor<any>>    
     open?:boolean
     className?:string
 }
 export default function Collapse({children, open, className }:CollapseProps) {
-    return(<div className={classNames('transition-all duration-200',{'h-auto min-h-0  overflow-visible':open, 'h-0 min-h-0 overflow-hidden ':!open})}>
-{children}
+    const childrenRef=useRef<HTMLElement>(null)
+    useEffect(()=>{
+      console.log(  childrenRef.current?.offsetWidth)
+    })
+    return(<div className={classNames('transition-all',{'h-auto min-h-0  overflow-visible':open, 'h-0 min-h-0 overflow-hidden ':!open})}>
+{/* {children} */}
+<>{React.cloneElement(children, { ref: childrenRef })}</>
     </div>)
 }
