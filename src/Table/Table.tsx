145ddeca5edf17f0afbb29b0787dd9437d8be8da
ardigc <agordo.95@gmail.com>
2023-07-07 10:09:49 +0200

    add classname

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -6,11 +6,14 @@ type TableReactPops = DetailedHTMLProps<
 >
 export interface TableProps extends TableReactPops {
   children: ReactNode
+  className?: string
 }
-export default function Table({ children }: TableProps) {
+export default function Table({ children, className }: TableProps) {
   return (
     <table
-      className={classNames('table w-full border-collapse border-spacing-0')}
+      className={classNames('table w-full border-collapse border-spacing-0', {
+        [className || '']: className,
+      })}
     >
       {children}
     </table>
