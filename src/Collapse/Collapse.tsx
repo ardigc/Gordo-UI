766c8f824f6c8ed34117e51c57307f0c2128713a
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-08 15:19:44 +0000

    add orientation

--- src/Collapse/Collapse.tsx
+++ src/Collapse/Collapse.tsx
@@ -1,25 +1,51 @@
-import classNames from "classnames"
-import React, { ElementType, JSXElementConstructor, ReactElement, ReactNode, useEffect, useRef, useState } from "react"
+import classNames from 'classnames'
+import React, {
+  ElementType,
+  JSXElementConstructor,
+  ReactElement,
+  ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 
-export interface CollapseProps{
-    children: ReactElement<any, string | JSXElementConstructor<any>>    
-    open?:boolean
-    className?:string
-    collapsedSize?: number|string
-    component?: ElementType
+export interface CollapseProps {
+  children: ReactElement<any, string | JSXElementConstructor<any>>
+  open?: boolean
+  className?: string
+  collapsedSize?: number | string
+  component?: ElementType
+  orientation?:	'horizontal'
+  | 'vertical'
 }
-export default function Collapse({children, open, className, collapsedSize, component}:CollapseProps) {
-    const childrenRef=useRef<HTMLElement>(null)
-    const [childrenHeight, setChildrenHeight] = useState(0)
-    useEffect(()=>{
-        if (childrenRef.current?.offsetHeight) {
-            
-            setChildrenHeight(  childrenRef.current?.offsetHeight)
-        }
-    },[open])
-const RenderComponent= component?component:'div'
-    return(<div style={{height:open?childrenHeight:collapsedSize? collapsedSize:0}} className={classNames('transition-all duration-300 overflow-hidden',{[className||'']:className,' min-h-0  ':open, 'min-h-0 ':!open})}>
-{/* {children} */}
-<>{React.cloneElement(children, { ref: childrenRef })}</>
-    </div>)
+export default function Collapse({
+  children,
+  open,
+  className,
+  collapsedSize,
+  component,
+}: CollapseProps) {
+  const childrenRef = useRef<HTMLElement>(null)
+  const [childrenHeight, setChildrenHeight] = useState(0)
+  useEffect(() => {
+    if (childrenRef.current?.offsetHeight) {
+      setChildrenHeight(childrenRef.current?.offsetHeight)
+    }
+  }, [open])
+  const RenderComponent = component ? component : 'div'
+  return (
+    <div
+      style={{
+        height: open ? childrenHeight : collapsedSize ? collapsedSize : 0,
+      }}
+      className={classNames('transition-all duration-300 overflow-hidden', {
+        [className || '']: className,
+        ' min-h-0  ': open,
+        'min-h-0 ': !open,
+      })}
+    >
+      {/* {children} */}
+      <>{React.cloneElement(children, { ref: childrenRef })}</>
+    </div>
+  )
 }
