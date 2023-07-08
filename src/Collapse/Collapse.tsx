f8c0c8b6e99a5ecd46211735cbf34907eb690ca8
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-08 15:02:26 +0000

    add collapse sized

--- src/Collapse/Collapse.tsx
+++ src/Collapse/Collapse.tsx
@@ -5,8 +5,9 @@ export interface CollapseProps{
     children: ReactElement<any, string | JSXElementConstructor<any>>    
     open?:boolean
     className?:string
+    collapsedSize?: number|string
 }
-export default function Collapse({children, open, className }:CollapseProps) {
+export default function Collapse({children, open, className, collapsedSize }:CollapseProps) {
     const childrenRef=useRef<HTMLElement>(null)
     const [childrenHeight, setChildrenHeight] = useState(0)
     useEffect(()=>{
@@ -15,7 +16,8 @@ export default function Collapse({children, open, className }:CollapseProps) {
             setChildrenHeight(  childrenRef.current?.offsetHeight)
         }
     },[open])
-    return(<div style={{height:open?childrenHeight:0}} className={classNames('transition-all duration-300 overflow-hidden',{' min-h-0  ':open, 'min-h-0 ':!open})}>
+
+    return(<div style={{height:open?childrenHeight:collapsedSize? collapsedSize:0}} className={classNames('transition-all duration-300 overflow-hidden',{' min-h-0  ':open, 'min-h-0 ':!open})}>
 {/* {children} */}
 <>{React.cloneElement(children, { ref: childrenRef })}</>
     </div>)
