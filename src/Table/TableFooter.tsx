c04047c03b10f3a5e1a04ae1a7476c1f5f02af0b
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-31 21:54:46 +0000

    creating inteface tablefooterprops

--- src/Table/TableFooter.tsx
+++ src/Table/TableFooter.tsx
@@ -1,4 +1,9 @@
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react"
 
+type TableFooterReactProps=DetailedHTMLProps<HTMLAttributes<HTMLTableSectionElement>,HTMLTableSectionElement>
+export interface TableFooterProps extends TableFooterReactProps{
+    children?: ReactNode
+}
 export default function TableFooter(params:TableFooterProps) {
     return(<tfoot>
         {children}
