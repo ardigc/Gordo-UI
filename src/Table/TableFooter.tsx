866f0062393e3b8ffbf40dffa57bb7a6e90d6d89
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-31 23:36:50 +0000

    add render component

--- src/Table/TableFooter.tsx
+++ src/Table/TableFooter.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames'
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
+import { DetailedHTMLProps, ElementType, HTMLAttributes, ReactNode } from 'react'
 
 type TableFooterReactProps = DetailedHTMLProps<
   HTMLAttributes<HTMLTableSectionElement>,
@@ -8,20 +8,23 @@ type TableFooterReactProps = DetailedHTMLProps<
 export interface TableFooterProps extends TableFooterReactProps {
   children?: ReactNode
   className?: string
+  component?:ElementType
 }
 export default function TableFooter({
   children,
   className,
+  component,
   ...rest
 }: TableFooterProps) {
+  const RenderComponent =component? component:'tfoot'
   return (
-    <tfoot
+    <RenderComponent
       className={classNames('table-footer-group', {
         [className || '']: className,
       })}
       {...rest}
     >
       {children}
-    </tfoot>
+    </RenderComponent>
   )
 }
