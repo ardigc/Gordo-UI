87c0f4c05f0949bb319765f70ecf9249c84e7eab
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-31 22:10:03 +0000

    creating tablefooter stories

---
+++ src/Table/TableFooter.stories.tsx
@@ -0,0 +1,104 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Table from './Table'
+import TableCell from './TableCell'
+import TableRow from './TableRow'
+import TableHead from './TableHead'
+import TableBody from './TableBody'
+import TableFooter, { TableFooterProps } from './TableFooter'
+import Paper from '../Paper/Paper'
+import { ReactNode } from 'react'
+import TableContainer from './TableContainer'
+const meta = {
+    component: TableFooter,
+    title: 'GordoUI/TableFooter',
+    tags: ['autodocs'],
+  } satisfies Meta<typeof TableFooter>
+  export default meta
+  type Story = StoryObj<typeof meta>
+  const TestFunc = ({ args }: { args: TableFooterProps }) => {
+    function createData(
+      name: string,
+      calories: number,
+      fat: number,
+      carbs: number,
+      protein: number
+    ) {
+      return { name, calories, fat, carbs, protein }
+    }
+  
+    const rows = [
+      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+      createData('Eclair', 262, 16.0, 24, 6.0),
+      createData('Cupcake', 305, 3.7, 67, 4.3),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ]
+  
+    return (
+      // <div className="h-48 overflow-auto">
+      <TableContainer
+        {...args}
+        component={({ children }: { children: ReactNode }) => (
+          <Paper elevation={10}>{children}</Paper>
+        )}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter><TableRow><TableCell>hola</TableCell></TableRow></TableFooter>
+        </Table>
+      </TableContainer>
+      // </div>
+    )
+  }
+  export const test: Story = {
+    render: (args) => <TestFunc args={args} />,
+    args: {
+      children: <div></div>,
+  
+      // className: 'rounded ',
+      // padding: 'normal',
+      // size: 'medium',
+      // stickyHeader: true,
+    },
+  }
+  
