15ed2aab28fffe28215d9a5fdfdcbb6ff93675b7
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-31 22:27:18 +0000

    fix classname to tablefooter

--- src/Table/TableFooter.tsx
+++ src/Table/TableFooter.tsx
@@ -1,14 +1,13 @@
 import classNames from "classnames"
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react"
-import { className } from "../FormControl/FormControl.stories"
 
 type TableFooterReactProps=DetailedHTMLProps<HTMLAttributes<HTMLTableSectionElement>,HTMLTableSectionElement>
 export interface TableFooterProps extends TableFooterReactProps{
     children?: ReactNode
     className?: string
 }
-export default function TableFooter({children,...rest}:TableFooterProps) {
-    return(<tfoot className={classNames('table-footer-group',[className||'']:className)} {...rest}>
+export default function TableFooter({children,className,...rest}:TableFooterProps) {
+    return(<tfoot className={classNames('table-footer-group',{[className||'']:className,})} {...rest}>
         {children}
     </tfoot>)
 }
