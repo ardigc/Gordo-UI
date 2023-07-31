0933feac46734fb7744802a6fa9f06c697042d93
ardigc <130089987+ardigc@users.noreply.github.com>
2023-07-31 22:29:24 +0000

    format

--- src/Table/TableFooter.tsx
+++ src/Table/TableFooter.tsx
@@ -1,13 +1,27 @@
-import classNames from "classnames"
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react"
+import classNames from 'classnames'
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 
-type TableFooterReactProps=DetailedHTMLProps<HTMLAttributes<HTMLTableSectionElement>,HTMLTableSectionElement>
-export interface TableFooterProps extends TableFooterReactProps{
-    children?: ReactNode
-    className?: string
+type TableFooterReactProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLTableSectionElement>,
+  HTMLTableSectionElement
+>
+export interface TableFooterProps extends TableFooterReactProps {
+  children?: ReactNode
+  className?: string
 }
-export default function TableFooter({children,className,...rest}:TableFooterProps) {
-    return(<tfoot className={classNames('table-footer-group',{[className||'']:className,})} {...rest}>
-        {children}
-    </tfoot>)
+export default function TableFooter({
+  children,
+  className,
+  ...rest
+}: TableFooterProps) {
+  return (
+    <tfoot
+      className={classNames('table-footer-group', {
+        [className || '']: className,
+      })}
+      {...rest}
+    >
+      {children}
+    </tfoot>
+  )
 }
