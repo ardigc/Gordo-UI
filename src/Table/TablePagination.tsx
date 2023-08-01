97bd914b2c3b94cad4fb5fb6e196ff2cb0a30332
ardigc <agordo.95@gmail.com>
2023-08-01 14:46:25 +0200

    creating input

--- src/Table/TablePagination.tsx
+++ src/Table/TablePagination.tsx
@@ -12,7 +12,8 @@ export default function TablePagination({}: TablePaginationProps) {
       <div className="flex relative items-center pl-6 pr-1 justify-end">
         <p className="my-4">Rows per page:</p>
         <div className="inline-flex ml-2 mr-8 items-center relative">
-          <div>xd</div>
+          <div className="overflow-hidden overflow-ellipsis block pl-2 pr-6 pt-1 pb-[5px] min-w-[16px] min-h-[1.4375em] "></div>
+          <input className="b-0 l-0 absolute"></input>
         </div>
       </div>
     </td>
