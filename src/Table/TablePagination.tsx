934eadb7bd59efc6f6f51c1979ce14a0c25fda53
ardigc <130089987+ardigc@users.noreply.github.com>
2023-08-01 03:39:44 +0000

    styling tablepagination

--- src/Table/TablePagination.tsx
+++ src/Table/TablePagination.tsx
@@ -10,6 +10,6 @@ export interface TablePaginationProps extends TableCellReactProps{
 }
 export default function TablePagination({}:TablePaginationProps) {
     return (<TableCell className="p-0" colSpan={1000}>
-        <div className="flex relative items-center pl-6 pr-1"><p className="my-4">Rows per page:</p></div>
+        <div className="flex relative items-center pl-6 pr-1 justify-end"><p className="my-4">Rows per page:</p></div>
         </TableCell>)
 }
