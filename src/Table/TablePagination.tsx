abcc87d407cd66cfa234dc76e30c1059933c5d2b
ardigc <130089987+ardigc@users.noreply.github.com>
2023-08-01 01:09:28 +0000

    testing

--- src/Table/TablePagination.tsx
+++ src/Table/TablePagination.tsx
@@ -10,6 +10,6 @@ export interface TablePaginationProps extends TableCellReactProps{
 }
 export default function TablePagination({}:TablePaginationProps) {
     return (<TableCell>
-        <div className="flex relative items-center "><p className="my-4">Rows per page:</p></div>)
-        </TableCell>
+        <div className="flex relative items-center "><p className="my-4">Rows per page:</p></div>
+        </TableCell>)
 }
