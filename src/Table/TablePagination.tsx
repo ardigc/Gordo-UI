3e751ddddc109fa259a62ab30afe9137e1af93b6
ardigc <agordo.95@gmail.com>
2023-08-01 14:13:47 +0200

    styling box

--- src/Table/TablePagination.tsx
+++ src/Table/TablePagination.tsx
@@ -1,15 +1,20 @@
-import { DetailedHTMLProps, TdHTMLAttributes } from "react";
-import TableCell from "./TableCell";
+import { DetailedHTMLProps, TdHTMLAttributes } from 'react'
+import TableCell from './TableCell'
 
 type TableCellReactProps = DetailedHTMLProps<
   TdHTMLAttributes<HTMLTableCellElement>,
   HTMLTableCellElement
 >
-export interface TablePaginationProps extends TableCellReactProps{
-
-}
-export default function TablePagination({}:TablePaginationProps) {
-    return (<TableCell className="p-0" colSpan={1000}>
-        <div className="flex relative items-center pl-6 pr-1 justify-end"><p className="my-4">Rows per page:</p></div>
-        </TableCell>)
+export interface TablePaginationProps extends TableCellReactProps {}
+export default function TablePagination({}: TablePaginationProps) {
+  return (
+    <td className="p-0" colSpan={1000}>
+      <div className="flex relative items-center pl-6 pr-1 justify-end">
+        <p className="my-4">Rows per page:</p>
+        <div className="inline-flex ml-2 mr-8 items-center relative">
+          <div>xd</div>
+        </div>
+      </div>
+    </td>
+  )
 }
