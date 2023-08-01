92738c152b2f4b2c2ef31f27ba77114faed85c7b
ardigc <agordo.95@gmail.com>
2023-08-01 15:36:09 +0200

    add arrow and switch arrow

--- src/Table/TablePagination.tsx
+++ src/Table/TablePagination.tsx
@@ -1,5 +1,5 @@
-import { DetailedHTMLProps, TdHTMLAttributes } from 'react'
-import { ArrowDownIcon } from '../components/icons/StarIcon'
+import { DetailedHTMLProps, TdHTMLAttributes, useState } from 'react'
+import { ArrowDownIcon, ArrowUpIcon } from '../components/icons/StarIcon'
 
 type TableCellReactProps = DetailedHTMLProps<
   TdHTMLAttributes<HTMLTableCellElement>,
@@ -7,15 +7,22 @@ type TableCellReactProps = DetailedHTMLProps<
 >
 export interface TablePaginationProps extends TableCellReactProps {}
 export default function TablePagination({}: TablePaginationProps) {
+  const [open, setOpen] = useState(false)
+  const onClickHandler = () => {
+    setOpen(!open)
+  }
   return (
     <td className="p-0" colSpan={1000}>
       <div className="flex relative items-center pl-6 pr-1 justify-end">
         <p className="my-4">Rows per page:</p>
-        <div className="inline-flex ml-2 mr-8 items-center relative cursor-pointer">
+        <div
+          className="inline-flex ml-2 mr-8 items-center relative cursor-pointer"
+          onClick={onClickHandler}
+        >
           <div className="overflow-hidden overflow-ellipsis block pl-2 pr-6 pt-1 pb-[5px] min-w-[16px] box-content  min-h-[20.125px] "></div>
           <input className="b-0 l-0 absolute w-full opacity-0 pointer-events-none"></input>
           <div className="absolute w-6 h-6 right-0 flex justify-center items-center ">
-            <ArrowDownIcon />
+            {!open ? <ArrowDownIcon /> : <ArrowUpIcon />}
           </div>
         </div>
       </div>
--- src/components/icons/StarIcon.tsx
+++ src/components/icons/StarIcon.tsx
@@ -60,3 +60,23 @@ export function ArrowDownIcon() {
     </svg>
   )
 }
+
+export function ArrowUpIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-caret-up"
+      width={15}
+      height={15}
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M18 14l-6-6-6 6h12" />
+    </svg>
+  )
+}
