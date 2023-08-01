b0c5b4a3f5fb5a120558c48068c7ce3caec6654b
ardigc <130089987+ardigc@users.noreply.github.com>
2023-08-01 00:54:43 +0000

    teting tablepagination stories

--- src/Table/TablePagination.stories.tsx
+++ src/Table/TablePagination.stories.tsx
@@ -10,96 +10,98 @@ import { ReactNode } from 'react'
 import TableContainer from './TableContainer'
 import TableFooter from './TableFooter'
 const meta = {
-    component: TablePagination,
-    title: 'GordoUI/TablePagination',
-    tags: ['autodocs'],
-  } satisfies Meta<typeof TablePagination>
-  export default meta
-  type Story = StoryObj<typeof meta>
-  const TestFunc = ({ args }: { args: TablePaginationProps }) => {
-    function createData(
-      name: string,
-      calories: number,
-      fat: number,
-      carbs: number,
-      protein: number
-    ) {
-      return { name, calories, fat, carbs, protein }
-    }
-  
-    const rows = [
-      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-      createData('Eclair', 262, 16.0, 24, 6.0),
-      createData('Cupcake', 305, 3.7, 67, 4.3),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ]
-  
-    return (
-      // <div className="h-48 overflow-auto">
-      <TableContainer
-       
-        component={({ children }: { children: ReactNode }) => (
-          <Paper elevation={10}>{children}</Paper>
-        )}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+  component: TablePagination,
+  title: 'GordoUI/TablePagination',
+  tags: ['autodocs'],
+} satisfies Meta<typeof TablePagination>
+export default meta
+type Story = StoryObj<typeof meta>
+const TestFunc = ({ args }: { args: TablePaginationProps }) => {
+  function createData(
+    name: string,
+    calories: number,
+    fat: number,
+    carbs: number,
+    protein: number
+  ) {
+    return { name, calories, fat, carbs, protein }
+  }
+
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ]
+
+  return (
+    // <div className="h-48 overflow-auto">
+    <TableContainer
+      component={({ children }: { children: ReactNode }) => (
+        <Paper elevation={10}>{children}</Paper>
+      )}
+    >
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter ><TableRow><TablePagination  {...args}>holiti</TablePagination></TableRow></TableFooter>
-        </Table>
-      </TableContainer>
-      // </div>
-    )
-  }
-  export const test: Story = {
-    render: (args) => <TestFunc args={args} />,
-    args: {
-      children: <div></div>,
-  
-      className: 'bg-gray-200 ',
-      // padding: 'normal',
-      // size: 'medium',
-      // stickyHeader: true,
-    },
-  }
-  
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination {...args}></TablePagination>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+    // </div>
+  )
+}
+export const test: Story = {
+  render: (args) => <TestFunc args={args} />,
+  args: {
+    children: <div></div>,
+
+    className: 'bg-gray-200 ',
+    // padding: 'normal',
+    // size: 'medium',
+    // stickyHeader: true,
+  },
+}
