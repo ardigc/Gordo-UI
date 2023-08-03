bdbce5073d738f3ef89a45355466cd67a7625b96
ardigc <agordo.95@gmail.com>
2023-08-03 14:05:00 +0200

    add last page icon

--- src/components/icons/StarIcon.tsx
+++ src/components/icons/StarIcon.tsx
@@ -177,3 +177,22 @@ export function FirstPageIcon() {
     </svg>
   )
 }
+export function LastPageIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-chevron-right-pipe"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M6 6l6 6-6 6M17 5v13" />
+    </svg>
+  )
+}
