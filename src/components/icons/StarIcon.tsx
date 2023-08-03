5938a1d5c54b89b85e58b2277b67a7cf20e6ef16
ardigc <agordo.95@gmail.com>
2023-08-03 14:03:39 +0200

    add first page icon

--- src/components/icons/StarIcon.tsx
+++ src/components/icons/StarIcon.tsx
@@ -158,3 +158,22 @@ export function NextPageIconDis() {
     </svg>
   )
 }
+export function FirstPageIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-chevron-left-pipe"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M7 6v12M18 6l-6 6 6 6" />
+    </svg>
+  )
+}
