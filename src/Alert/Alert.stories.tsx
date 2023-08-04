d40fdee7b69f4333432f0516ef559eb6c4dbd43b
ardigc <agordo.95@gmail.com>
2023-08-04 11:46:57 +0200

    creating stories

---
+++ src/Alert/Alert.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Alert from './Alert'
+
+const meta = {
+  component: Alert,
+  title: 'GordoUI/Alert',
+  tags: ['autodocs'],
+} satisfies Meta<typeof Alert>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const primary: Story = {
+  args: {
+    children: 'hola',
+  },
+}
