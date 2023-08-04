46980f817bc5ac3a70110d89d667b8e5a5e5cb62
ardigc <agordo.95@gmail.com>
2023-08-04 12:32:58 +0200

    styling alert

--- src/Alert/Alert.tsx
+++ src/Alert/Alert.tsx
@@ -13,13 +13,19 @@ export default function Alert({ children, severity = 'success' }: AlertProps) {
       className={classNames(
         'flex py-[6px] px-4 font-base text-sm font-normal',
         {
-          'bg-green-200 text-green-800': severity === 'success',
+          'bg-green-100 text-green-800': severity === 'success',
         }
       )}
       elevation={0}
     >
-      {severity === 'success' && <SuccessIcon />}
-      <div className={classNames({ 'text-green-800': severity === 'success' })}>
+      <div className={classNames('mr-[12px] py-[7px]')}>
+        {severity === 'success' && <SuccessIcon />}
+      </div>
+      <div
+        className={classNames('py-2', {
+          'text-green-800': severity === 'success',
+        })}
+      >
         {children}
       </div>
     </Paper>
