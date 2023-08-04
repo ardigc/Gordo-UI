8f44275f30afabe77d87d4f86a410066733f7336
ardigc <agordo.95@gmail.com>
2023-08-04 12:20:16 +0200

    styling paper

--- src/Alert/Alert.tsx
+++ src/Alert/Alert.tsx
@@ -9,9 +9,12 @@ export interface AlertProps extends Omit<PaperProps, 'children'> {
 export default function Alert({ children, severity = 'success' }: AlertProps) {
   return (
     <Paper
-      className={classNames('flex py-[6px] px-4', {
-        'bg-green-200': severity === 'success',
-      })}
+      className={classNames(
+        'flex py-[6px] px-4 font-base text-sm font-normal',
+        {
+          'bg-green-200': severity === 'success',
+        }
+      )}
       elevation={0}
     >
       <div className={classNames({ 'text-green-800': severity === 'success' })}>
