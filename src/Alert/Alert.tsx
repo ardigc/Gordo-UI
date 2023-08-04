0fa58c4b6dd2034691f7c675e164fbabee0de8eb
ardigc <agordo.95@gmail.com>
2023-08-04 12:29:07 +0200

    add success icon

--- src/Alert/Alert.tsx
+++ src/Alert/Alert.tsx
@@ -1,6 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 import Paper, { PaperProps } from '../Paper/Paper'
 import classNames from 'classnames'
+import { SuccessIcon } from '../components/icons/Icons'
 
 export interface AlertProps extends Omit<PaperProps, 'children'> {
   children?: ReactNode
@@ -12,11 +13,12 @@ export default function Alert({ children, severity = 'success' }: AlertProps) {
       className={classNames(
         'flex py-[6px] px-4 font-base text-sm font-normal',
         {
-          'bg-green-200': severity === 'success',
+          'bg-green-200 text-green-800': severity === 'success',
         }
       )}
       elevation={0}
     >
+      {severity === 'success' && <SuccessIcon />}
       <div className={classNames({ 'text-green-800': severity === 'success' })}>
         {children}
       </div>
--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -272,3 +272,23 @@ export function ArrowUp() {
     </svg>
   )
 }
+export function SuccessIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-circle-check"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      stroke-width={2}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+      <path d="M9 12l2 2l4 -4"></path>
+    </svg>
+  )
+}
