15b0f49e27e0ac9dbc1962b65f40bfc3411c84dd
ardigc <agordo.95@gmail.com>
2023-08-04 12:12:55 +0200

    add severity

--- src/Alert/Alert.tsx
+++ src/Alert/Alert.tsx
@@ -1,13 +1,20 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 import Paper, { PaperProps } from '../Paper/Paper'
+import classNames from 'classnames'
 
 export interface AlertProps extends Omit<PaperProps, 'children'> {
   children?: ReactNode
+  severity?: 'error' | 'info' | 'success' | 'warning'
 }
-export default function Alert({ children }: AlertProps) {
+export default function Alert({ children, severity = 'success' }: AlertProps) {
   return (
-    <Paper>
-      <div>{children}</div>
+    <Paper
+      className={classNames({ 'bg-green-200': severity === 'success' })}
+      elevation={0}
+    >
+      <div className={classNames({ 'text-green-800': severity === 'success' })}>
+        {children}
+      </div>
     </Paper>
   )
 }
