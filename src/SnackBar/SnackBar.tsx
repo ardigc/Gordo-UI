dd6e82a116455473dd3779f151b061a6c21f94e0
ardigc <agordo.95@gmail.com>
2023-08-05 12:02:45 +0200

    stylin @media

--- src/SnackBar/SnackBar.tsx
+++ src/SnackBar/SnackBar.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
+import Paper from '../Paper/Paper'
 
 type DivReactProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
@@ -18,11 +19,11 @@ export default function SnackBar({
   return (
     <div
       className={classNames(
-        'fixed bottom-6 left-6 right-auto z-50 flex justify-start items-center'
+        'fixed bottom-2 left-2 right-2 z-50 flex justify-start items-center sm:bottom-6 sm:left-6 sm:right-auto'
       )}
       {...rest}
     >
-      {message}
+      <Paper>{message}</Paper>
     </div>
   )
 }
