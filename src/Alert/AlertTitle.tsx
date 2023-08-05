3664cfcc5791f0635044f15a239bd3578c492108
ardigc <agordo.95@gmail.com>
2023-08-05 11:24:21 +0200

    add class name

--- src/Alert/AlertTitle.tsx
+++ src/Alert/AlertTitle.tsx
@@ -7,12 +7,18 @@ type DivReactProps = DetailedHTMLProps<
 >
 export interface AlertTitleProps extends DivReactProps {
   children: ReactNode
+  className?: string
 }
-export default function AlertTitle({ children, ...rest }: AlertTitleProps) {
+export default function AlertTitle({
+  children,
+  className,
+  ...rest
+}: AlertTitleProps) {
   return (
     <div
       className={classNames(
-        '-mt-[2px] mb-[5.6px] font-base font-medium text-base'
+        '-mt-[2px] mb-[5.6px] font-base font-medium text-base',
+        { [className || '']: className }
       )}
       {...rest}
     >
