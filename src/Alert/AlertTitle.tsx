b81d5c49e04ab8f82aaad597f83ea61b8783e8fb
ardigc <agordo.95@gmail.com>
2023-08-05 11:10:49 +0200

    add children

--- src/Alert/AlertTitle.tsx
+++ src/Alert/AlertTitle.tsx
@@ -1,10 +1,12 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react'
 
 type DivReactProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >
-export interface AlertTitleProps extends DivReactProps {}
-export default function AlertTitle({}: AlertTitleProps) {
-  return <div></div>
+export interface AlertTitleProps extends DivReactProps {
+  children: ReactNode
+}
+export default function AlertTitle({ children, ...rest }: AlertTitleProps) {
+  return <div {...rest}>{children}</div>
 }
