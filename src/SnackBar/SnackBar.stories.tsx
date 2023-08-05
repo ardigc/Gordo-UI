9d07c216a673d1242b146f4bdb9a4f2c38a64df6
ardigc <130089987+ardigc@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    testing children

--- src/SnackBar/SnackBar.stories.tsx
+++ src/SnackBar/SnackBar.stories.tsx
@@ -3,6 +3,7 @@ import SnackBar, { SnackBarProps } from './SnackBar'
 import { useState } from 'react'
 import Button from '../Button/Button'
 import { XIcon } from '../components/icons/Icons'
+import Alert from '../Alert/Alert'
 
 const meta = {
   component: SnackBar,
@@ -54,5 +55,30 @@ export const autoHide: Story = {
   action:<Button><XIcon/></Button>,
     message: 'Note archived',
     autoHideDuration:600
+  },
+}
+const ChildrenFunc = ({ args }: { args: SnackBarProps }) => {
+  const [open, setOpen] = useState(false)
+  const handleClose = (_event: React.SyntheticEvent | Event|null, reason?: string) => {
+  
+console.log(reason)
+// if (reason === 'clickAway') {
+//   return;
+// }
+    setOpen(false);
+  };
+  return (
+    <>
+      <Button onClick={() => setOpen(!open)}>Hazme click</Button>
+      <SnackBar open={open} {...args} onClose={handleClose}><Alert>ESto es una prueba</Alert></SnackBar>
+    </>
+  )
+}
+export const children: Story = {
+  render: (args) => <ChildrenFunc args={args} />,
+  args: {
+  action:<Button><XIcon/></Button>,
+    message: 'Note archived',
+    autoHideDuration:6000
   },
 }
