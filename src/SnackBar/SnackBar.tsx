fe0a17e452eb08a311669659ba7a41415966f33c
ardigc <agordo.95@gmail.com>
2023-08-05 12:04:56 +0200

    add font to paper

--- src/SnackBar/SnackBar.tsx
+++ src/SnackBar/SnackBar.tsx
@@ -23,7 +23,9 @@ export default function SnackBar({
       )}
       {...rest}
     >
-      <Paper>{message}</Paper>
+      <Paper className={classNames('font-base font-normal text-base')}>
+        {message}
+      </Paper>
     </div>
   )
 }
