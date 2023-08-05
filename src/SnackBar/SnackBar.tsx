c21f1d8e3ddf64f5e04e44f10d449530c2555688
ardigc <130089987+ardigc@users.noreply.github.com>
2023-08-05 17:05:15 +0000

    fixing

--- src/SnackBar/SnackBar.tsx
+++ src/SnackBar/SnackBar.tsx
@@ -20,9 +20,12 @@ export interface SnackBarProps extends DivReactProps {
   message?: ReactNode
   open?: boolean
   action?: ReactNode
-  onClose?:(ev:Event|KeyboardEvent|null, reason:string) => void
-  autoHideDuration?:number
-  anchorOrigin?:{ horizontal: 'center' | 'left' | 'right', vertical: 'bottom' | 'top' }
+  onClose?: (ev: Event | KeyboardEvent | null, reason: string) => void
+  autoHideDuration?: number
+  anchorOrigin?: {
+    horizontal: 'center' | 'left' | 'right'
+    vertical: 'bottom' | 'top'
+  }
 }
 
 export default function SnackBar({
@@ -32,17 +35,17 @@ export default function SnackBar({
   action,
   onClose,
   autoHideDuration,
-  anchorOrigin={ vertical: 'bottom', horizontal: 'left' },
+  anchorOrigin = { vertical: 'bottom', horizontal: 'left' },
   ...rest
 }: SnackBarProps) {
   const [visible, setVisible] = useState(false)
   const timeOutRef = useRef<NodeJS.Timeout | null>(null)
-  if (autoHideDuration&&!timeOutRef.current) {
-    timeOutRef.current=setTimeout(() => {
-      timeOutRef.current=null
+  if (autoHideDuration && !timeOutRef.current) {
+    timeOutRef.current = setTimeout(() => {
+      timeOutRef.current = null
       if (!onClose) return
-      onClose(null,'timeout')
-    }, autoHideDuration);
+      onClose(null, 'timeout')
+    }, autoHideDuration)
   }
 
   useEffect(() => {
@@ -50,60 +53,70 @@ export default function SnackBar({
       setVisible(Boolean(open))
     }
 
-    const handleKeyDown = (event:KeyboardEvent) => {
+    const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
         if (open && onClose) {
-          onClose(event, 'escapeKeyDown');
+          onClose(event, 'escapeKeyDown')
         }
       }
-    };
-    const handleKeyDownListener = (event: Event) => handleKeyDown(event as unknown as KeyboardEvent);
-    document.addEventListener('keydown', handleKeyDownListener);
+    }
+    const handleKeyDownListener = (event: Event) =>
+      handleKeyDown(event as unknown as KeyboardEvent)
+    document.addEventListener('keydown', handleKeyDownListener)
 
-    if(timeOutRef.current&&!open){
+    if (timeOutRef.current && !open) {
       clearTimeout(timeOutRef.current)
-      timeOutRef.current=null
+      timeOutRef.current = null
     }
     return () => {
-      document.removeEventListener('keydown', handleKeyDownListener);
-    };
-    
+      document.removeEventListener('keydown', handleKeyDownListener)
+    }
   }, [open])
 
-const clickAwayHandle= (ev:MouseEvent|TouchEvent)=>{
-  if (!onClose) {
-    return
+  const clickAwayHandle = (ev: MouseEvent | TouchEvent) => {
+    if (!onClose) {
+      return
+    }
+    onClose(ev, 'clickAway')
   }
-onClose(ev,'clickAway')
-}
   return (
     <>
       {visible && (
         <Clickaway onClickaway={clickAwayHandle}>
-        <div
-          className={classNames(
-            ' z-50 flex justify-start items-center fixed  animate-opacity',
-            { 'animate-opacity0 opacity-0': !open,
-          ' left-2 right-2  sm:left-6 sm:right-auto':  anchorOrigin.horizontal ==='left', 
-          ' left-2 right-2  sm:left-1/2 sm:right-auto sm:-translate-x-1/2':  anchorOrigin.horizontal ==='center', 
-          ' left-2 right-2  sm:left-auto sm:right-6':  anchorOrigin.horizontal ==='right', 
-          'sm:bottom-6 bottom-2 ':anchorOrigin.vertical=== 'bottom'
-          }
+          <div
+            className={classNames(
+              ' z-50 flex justify-start items-center fixed  animate-opacity',
+              {
+                'animate-opacity0 opacity-0': !open,
+                ' left-2 right-2  sm:left-6 sm:right-auto':
+                  anchorOrigin.horizontal === 'left',
+                ' left-2 right-2  sm:left-1/2 sm:right-auto sm:-translate-x-1/2':
+                  anchorOrigin.horizontal === 'center',
+                ' left-2 right-2  sm:left-auto sm:right-6':
+                  anchorOrigin.horizontal === 'right',
+                'sm:bottom-6 bottom-2 ': anchorOrigin.vertical === 'bottom',
+              }
             )}
             onAnimationEnd={() => setVisible(Boolean(open))}
             {...rest}
-            >
-              {children&&children}
-{       !children&&   <Paper
-            className={classNames(
-              'font-base font-normal text-base text-white grow bg-[#323232] flex px-4 py-[6px] items-center flex-wrap sm:min-w-[288px] sm:grow-[initial]'
-              )}
+          >
+            {children && children}
+            {!children && (
+              <Paper
+                className={classNames(
+                  'font-base font-normal text-base text-white grow bg-[#323232] flex px-4 py-[6px] items-center flex-wrap sm:min-w-[288px] sm:grow-[initial]'
+                )}
               >
-            <div className={classNames('py-2')}>{message}</div>
-            {action&&<div className='flex items-center ml-auto pl-4 -mr-2'>{action}</div>}
-          </Paper>}
-        </div>
-      </Clickaway>
+                <div className={classNames('py-2')}>{message}</div>
+                {action && (
+                  <div className="flex items-center ml-auto pl-4 -mr-2">
+                    {action}
+                  </div>
+                )}
+              </Paper>
+            )}
+          </div>
+        </Clickaway>
       )}
     </>
   )
